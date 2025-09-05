5d77af9cbb89a6b28289b395325a638ffcc3ec49
parth <parthgirdhar2004@gmail.com>
2025-09-05 19:39:26 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+import Lottery from "./Lottery";
+import {sum} from "./helper";
+
+function App(){
+    let winCondition=(ticket)=>{
+        return sum(ticket)==15;
+        //ticket.every((num)=>num===ticket[0]);
+        //ticket[0]===0;
+    }
+    return(
+        <>
+        <Lottery n={3} winCondition={winCondition}/>
+        </>
+    );
+}
+export default App;
